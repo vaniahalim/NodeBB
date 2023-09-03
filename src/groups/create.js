7ae9cd4695d6e1376612a5daf1fce8348ed86efd
Vania Halim <vhalim@andrew.cmu.edu>
2023-09-03 15:29:43 -0400

    updated conversion of groups/create.js from JS to TS

--- src/groups/create.js
+++ src/groups/create.js
@@ -36,93 +36,93 @@ const meta = __importStar(require("../meta"));
 const plugins = __importStar(require("../plugins"));
 const slugify = __importStar(require("../slugify"));
 const db = __importStar(require("../database"));
-function default_1(Groups) {
-    Groups.create = function (data) {
-        return __awaiter(this, void 0, void 0, function* () {
-            function isSystemGroup(data) {
-                return data.system === true || (typeof data.system === 'number' && data.system === 1) ||
-                    Groups.systemGroups.includes(data.name) ||
-                    Groups.isPrivilegeGroup(data.name);
-            }
-            const isSystem = isSystemGroup(data);
-            const timestamp = data.timestamp || Date.now();
-            const disableJoinRequestsString = typeof data.disableJoinRequests === 'string' ? data.disableJoinRequests : '0';
-            const disableJoinRequests = parseInt(disableJoinRequestsString, 10) === 1 ? 1 : 0;
-            const disableLeaveString = typeof data.disableLeave === 'string' ? data.disableLeave : '0';
-            const disableLeave = parseInt(disableLeaveString, 10) === 1 ? 1 : 0;
-            const isHidden = (typeof data.hidden === 'string') ?
-                (parseInt(data.hidden, 10) === 1) : false;
-            Groups.validateGroupName(data.name);
-            const exists = yield meta.userOrGroupExists(data.name);
-            if (exists) {
-                throw new Error('[[error:group-already-exists]]');
-            }
-            const memberCount = data.hasOwnProperty('ownerUid') ? 1 : 0;
-            const isPrivate = data.hasOwnProperty('private') ? (parseInt(data.private.toString(), 10) === 1) : true;
-            const userTitleEnabledString = typeof data.userTitleEnabled === 'string' ? data.userTitleEnabled : '0';
-            let groupData = {
-                name: data.name,
-                slug: slugify(data.name),
-                createtime: timestamp,
-                userTitle: data.userTitle || data.name,
-                userTitleEnabled: parseInt(userTitleEnabledString, 10) === 1 ? 1 : 0,
-                description: data.description || '',
-                memberCount: memberCount,
-                hidden: isHidden ? 1 : 0,
-                system: isSystem ? 1 : 0,
-                private: isPrivate ? 1 : 0,
-                disableJoinRequests: disableJoinRequests,
-                disableLeave: disableLeave,
-            };
-            yield plugins.hooks.fire('filter:group.create', { group: groupData, data: data });
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            yield db.sortedSetAdd('groups:createtime', groupData.createtime, groupData.name);
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            yield db.setObject(`group:${groupData.name}`, groupData);
-            if (data.hasOwnProperty('ownerUid')) {
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                yield db.setAdd(`group:${groupData.name}:owners`, data.ownerUid.toString());
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                yield db.sortedSetAdd(`group:${groupData.name}:members`, timestamp, data.ownerUid.toString());
-            }
-            if (!isHidden && !isSystem) {
-                const sortedSetAddBulkData = [
-                    ['groups:visible:createtime', timestamp, groupData.name],
-                    ['groups:visible:memberCount', groupData.memberCount, groupData.name],
-                    ['groups:visible:name', 0, `${groupData.name.toLowerCase()}:${groupData.name}`],
-                ];
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                yield db.sortedSetAddBulk(sortedSetAddBulkData);
-            }
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            yield db.setObjectField('groupslug:groupname', groupData.slug, groupData.name);
-            groupData = yield Groups.getGroupData(groupData.name);
-            try {
-                yield plugins.hooks.fire('action:group.create', { group: groupData });
-            }
-            catch (error) {
-                console.error('Error occurred during action:group.create:', error);
-            }
-            return groupData;
-        });
-    };
-    Groups.validateGroupName = function (name) {
-        if (!name) {
-            throw new Error('[[error:group-name-too-short]]');
+const Groups = {};
+Groups.create = function (data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        function isSystemGroup(data) {
+            return data.system === true || (typeof data.system === 'number' && data.system === 1) ||
+                Groups.systemGroups.includes(data.name) ||
+                Groups.isPrivilegeGroup(data.name);
         }
-        if (typeof name !== 'string') {
-            throw new Error('[[error:invalid-group-name]]');
+        const isSystem = isSystemGroup(data);
+        const timestamp = data.timestamp || Date.now();
+        const disableJoinRequestsString = typeof data.disableJoinRequests === 'string' ? data.disableJoinRequests : '0';
+        const disableJoinRequests = parseInt(disableJoinRequestsString, 10) === 1 ? 1 : 0;
+        const disableLeaveString = typeof data.disableLeave === 'string' ? data.disableLeave : '0';
+        const disableLeave = parseInt(disableLeaveString, 10) === 1 ? 1 : 0;
+        const isHidden = (typeof data.hidden === 'string') ?
+            (parseInt(data.hidden, 10) === 1) : false;
+        Groups.validateGroupName(data.name);
+        const exists = yield meta.userOrGroupExists(data.name);
+        if (exists) {
+            throw new Error('[[error:group-already-exists]]');
+        }
+        const memberCount = data.hasOwnProperty('ownerUid') ? 1 : 0;
+        const isPrivate = data.hasOwnProperty('private') ? (parseInt(data.private.toString(), 10) === 1) : true;
+        const userTitleEnabledString = typeof data.userTitleEnabled === 'string' ? data.userTitleEnabled : '0';
+        let groupData = {
+            name: data.name,
+            slug: slugify(data.name),
+            createtime: timestamp,
+            userTitle: data.userTitle || data.name,
+            userTitleEnabled: parseInt(userTitleEnabledString, 10) === 1 ? 1 : 0,
+            description: data.description || '',
+            memberCount: memberCount,
+            hidden: isHidden ? 1 : 0,
+            system: isSystem ? 1 : 0,
+            private: isPrivate ? 1 : 0,
+            disableJoinRequests: disableJoinRequests,
+            disableLeave: disableLeave,
+        };
+        yield plugins.hooks.fire('filter:group.create', { group: groupData, data: data });
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        yield db.sortedSetAdd('groups:createtime', groupData.createtime, groupData.name);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        yield db.setObject(`group:${groupData.name}`, groupData);
+        if (data.hasOwnProperty('ownerUid')) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            yield db.setAdd(`group:${groupData.name}:owners`, data.ownerUid.toString());
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            yield db.sortedSetAdd(`group:${groupData.name}:members`, timestamp, data.ownerUid.toString());
+        }
+        if (!isHidden && !isSystem) {
+            const sortedSetAddBulkData = [
+                ['groups:visible:createtime', timestamp, groupData.name],
+                ['groups:visible:memberCount', groupData.memberCount, groupData.name],
+                ['groups:visible:name', 0, `${groupData.name.toLowerCase()}:${groupData.name}`],
+            ];
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            yield db.sortedSetAddBulk(sortedSetAddBulkData);
         }
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        if (!Groups.isPrivilegeGroup(name) && name.length > meta.config.maximumGroupNameLength) {
-            throw new Error('[[error:group-name-too-long]]');
+        yield db.setObjectField('groupslug:groupname', groupData.slug, groupData.name);
+        groupData = yield Groups.getGroupData(groupData.name);
+        try {
+            yield plugins.hooks.fire('action:group.create', { group: groupData });
         }
-        if (name === 'guests' || (!Groups.isPrivilegeGroup(name) && name.includes(':'))) {
-            throw new Error('[[error:invalid-group-name]]');
+        catch (error) {
+            console.error('Error occurred during action:group.create:', error);
         }
-        if (name.includes('/') || !slugify(name)) {
-            throw new Error('[[error:invalid-group-name]]');
-        }
-    };
-}
-exports.default = default_1;
+        return groupData;
+    });
+};
+Groups.validateGroupName = function (name) {
+    if (!name) {
+        throw new Error('[[error:group-name-too-short]]');
+    }
+    if (typeof name !== 'string') {
+        throw new Error('[[error:invalid-group-name]]');
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    if (!Groups.isPrivilegeGroup(name) && name.length > meta.config.maximumGroupNameLength) {
+        throw new Error('[[error:group-name-too-long]]');
+    }
+    if (name === 'guests' || (!Groups.isPrivilegeGroup(name) && name.includes(':'))) {
+        throw new Error('[[error:invalid-group-name]]');
+    }
+    if (name.includes('/') || !slugify(name)) {
+        throw new Error('[[error:invalid-group-name]]');
+    }
+};
+exports.default = Groups;
+// converted with help from ChatGPT
--- src/groups/create.ts
+++ src/groups/create.ts
@@ -42,107 +42,108 @@ interface GroupsInstance {
     slug: string;
 }
 
-export default function (Groups: GroupsStatic) {
-    Groups.create = async function (data: GroupsInstance) {
-        function isSystemGroup(data: GroupsInstance): boolean {
-            return data.system === true || (typeof data.system === 'number' && data.system === 1) ||
-                Groups.systemGroups.includes(data.name) ||
-                Groups.isPrivilegeGroup(data.name);
-        }
-        const isSystem = isSystemGroup(data);
-        const timestamp = data.timestamp || Date.now();
-        const disableJoinRequestsString =
-        typeof data.disableJoinRequests === 'string' ? data.disableJoinRequests : '0';
-        const disableJoinRequests: number =
-            parseInt(disableJoinRequestsString, 10) === 1 ? 1 : 0;
-        const disableLeaveString =
-            typeof data.disableLeave === 'string' ? data.disableLeave : '0';
-        const disableLeave: number =
-            parseInt(disableLeaveString, 10) === 1 ? 1 : 0;
+const Groups : GroupsStatic = {} as GroupsStatic;
+Groups.create = async function (data: GroupsInstance) {
+    function isSystemGroup(data: GroupsInstance): boolean {
+        return data.system === true || (typeof data.system === 'number' && data.system === 1) ||
+            Groups.systemGroups.includes(data.name) ||
+            Groups.isPrivilegeGroup(data.name);
+    }
+    const isSystem = isSystemGroup(data);
+    const timestamp = data.timestamp || Date.now();
+    const disableJoinRequestsString =
+    typeof data.disableJoinRequests === 'string' ? data.disableJoinRequests : '0';
+    const disableJoinRequests: number =
+        parseInt(disableJoinRequestsString, 10) === 1 ? 1 : 0;
+    const disableLeaveString =
+        typeof data.disableLeave === 'string' ? data.disableLeave : '0';
+    const disableLeave: number =
+        parseInt(disableLeaveString, 10) === 1 ? 1 : 0;
 
-        const isHidden: boolean = (typeof data.hidden === 'string') ?
-            (parseInt(data.hidden, 10) === 1) : false;
+    const isHidden: boolean = (typeof data.hidden === 'string') ?
+        (parseInt(data.hidden, 10) === 1) : false;
 
-        Groups.validateGroupName(data.name);
+    Groups.validateGroupName(data.name);
 
-        const exists = await meta.userOrGroupExists(data.name) as boolean;
-        if (exists) {
-            throw new Error('[[error:group-already-exists]]');
-        }
+    const exists = await meta.userOrGroupExists(data.name) as boolean;
+    if (exists) {
+        throw new Error('[[error:group-already-exists]]');
+    }
 
-        const memberCount = data.hasOwnProperty('ownerUid') ? 1 : 0;
-        const isPrivate = data.hasOwnProperty('private') ? (parseInt(data.private.toString(), 10) === 1) : true;
-        const userTitleEnabledString = typeof data.userTitleEnabled === 'string' ? data.userTitleEnabled : '0';
-        let groupData: GroupData = {
-            name: data.name,
-            slug: slugify(data.name) as string,
-            createtime: timestamp,
-            userTitle: data.userTitle || data.name,
-            userTitleEnabled: parseInt(userTitleEnabledString, 10) === 1 ? 1 : 0,
-            description: data.description || '',
-            memberCount: memberCount,
-            hidden: isHidden ? 1 : 0,
-            system: isSystem ? 1 : 0,
-            private: isPrivate ? 1 : 0,
-            disableJoinRequests: disableJoinRequests,
-            disableLeave: disableLeave,
-        };
-
-        await plugins.hooks.fire('filter:group.create', { group: groupData, data: data });
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        await db.sortedSetAdd('groups:createtime', groupData.createtime, groupData.name);
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        await db.setObject(`group:${groupData.name}`, groupData);
-
-        if (data.hasOwnProperty('ownerUid')) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            await db.setAdd(`group:${groupData.name}:owners`, data.ownerUid.toString());
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            await db.sortedSetAdd(`group:${groupData.name}:members`, timestamp, data.ownerUid.toString());
-        }
-
-        if (!isHidden && !isSystem) {
-            const sortedSetAddBulkData = [
-                ['groups:visible:createtime', timestamp, groupData.name],
-                ['groups:visible:memberCount', groupData.memberCount, groupData.name],
-                ['groups:visible:name', 0, `${groupData.name.toLowerCase()}:${groupData.name}`],
-            ];
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            await db.sortedSetAddBulk(sortedSetAddBulkData);
-        }
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        await db.setObjectField('groupslug:groupname', groupData.slug, groupData.name);
-
-        groupData = await Groups.getGroupData(groupData.name);
-        try {
-            await plugins.hooks.fire('action:group.create', { group: groupData });
-        } catch (error) {
-            console.error('Error occurred during action:group.create:', error);
-        }
-        return groupData;
+    const memberCount = data.hasOwnProperty('ownerUid') ? 1 : 0;
+    const isPrivate = data.hasOwnProperty('private') ? (parseInt(data.private.toString(), 10) === 1) : true;
+    const userTitleEnabledString = typeof data.userTitleEnabled === 'string' ? data.userTitleEnabled : '0';
+    let groupData: GroupData = {
+        name: data.name,
+        slug: slugify(data.name) as string,
+        createtime: timestamp,
+        userTitle: data.userTitle || data.name,
+        userTitleEnabled: parseInt(userTitleEnabledString, 10) === 1 ? 1 : 0,
+        description: data.description || '',
+        memberCount: memberCount,
+        hidden: isHidden ? 1 : 0,
+        system: isSystem ? 1 : 0,
+        private: isPrivate ? 1 : 0,
+        disableJoinRequests: disableJoinRequests,
+        disableLeave: disableLeave,
     };
 
-    Groups.validateGroupName = function (name: string) {
-        if (!name) {
-            throw new Error('[[error:group-name-too-short]]');
-        }
+    await plugins.hooks.fire('filter:group.create', { group: groupData, data: data });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    await db.sortedSetAdd('groups:createtime', groupData.createtime, groupData.name);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    await db.setObject(`group:${groupData.name}`, groupData);
 
-        if (typeof name !== 'string') {
-            throw new Error('[[error:invalid-group-name]]');
-        }
+    if (data.hasOwnProperty('ownerUid')) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        if (!Groups.isPrivilegeGroup(name) && name.length > meta.config.maximumGroupNameLength) {
-            throw new Error('[[error:group-name-too-long]]');
-        }
+        await db.setAdd(`group:${groupData.name}:owners`, data.ownerUid.toString());
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        await db.sortedSetAdd(`group:${groupData.name}:members`, timestamp, data.ownerUid.toString());
+    }
 
-        if (name === 'guests' || (!Groups.isPrivilegeGroup(name) && name.includes(':'))) {
-            throw new Error('[[error:invalid-group-name]]');
-        }
+    if (!isHidden && !isSystem) {
+        const sortedSetAddBulkData = [
+            ['groups:visible:createtime', timestamp, groupData.name],
+            ['groups:visible:memberCount', groupData.memberCount, groupData.name],
+            ['groups:visible:name', 0, `${groupData.name.toLowerCase()}:${groupData.name}`],
+        ];
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        await db.sortedSetAddBulk(sortedSetAddBulkData);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    await db.setObjectField('groupslug:groupname', groupData.slug, groupData.name);
 
-        if (name.includes('/') || !slugify(name)) {
-            throw new Error('[[error:invalid-group-name]]');
-        }
-    };
-}
+    groupData = await Groups.getGroupData(groupData.name);
+    try {
+        await plugins.hooks.fire('action:group.create', { group: groupData });
+    } catch (error) {
+        console.error('Error occurred during action:group.create:', error);
+    }
+    return groupData;
+};
+
+Groups.validateGroupName = function (name: string) {
+    if (!name) {
+        throw new Error('[[error:group-name-too-short]]');
+    }
+
+    if (typeof name !== 'string') {
+        throw new Error('[[error:invalid-group-name]]');
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    if (!Groups.isPrivilegeGroup(name) && name.length > meta.config.maximumGroupNameLength) {
+        throw new Error('[[error:group-name-too-long]]');
+    }
+
+    if (name === 'guests' || (!Groups.isPrivilegeGroup(name) && name.includes(':'))) {
+        throw new Error('[[error:invalid-group-name]]');
+    }
+
+    if (name.includes('/') || !slugify(name)) {
+        throw new Error('[[error:invalid-group-name]]');
+    }
+};
+
+export default Groups;
 
 // converted with help from ChatGPT
